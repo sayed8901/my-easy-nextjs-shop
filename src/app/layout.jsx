deab82b7d149965007871903159ded0b64034b70
sayed8901 <sayed91515@gmail.com>
2023-08-15 22:48:32 +0600

    first commit

---
+++ src/app/layout.jsx
@@ -0,0 +1,39 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import classNames from "@/utils/classNames";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+  weight: ["100", "300", "400", "500", "700", "900"],
+  subsets: ["latin"],
+  variable: "--font-roboto",
+});
+
+export const metadata = {
+  title: "Easy Shop",
+  description: "Easy Shop is an e-commerce web application",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en" data-theme="dark" className="transition-all">
+      <body
+        className={classNames(
+          roboto.variable,
+          "container mx-auto px-2 font-roboto lg:px-10"
+        )}
+      >
+        <div className="flex min-h-screen flex-col justify-between">
+          <div>
+            <Navbar />
+            <main className="mt-5">{children}</main>
+          </div>
+          <Footer />
+        </div>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
