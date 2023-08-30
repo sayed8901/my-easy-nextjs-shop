cb1a894d89bff32887c671f843da904281a6ccf8
sayed8901 <sayed91515@gmail.com>
2023-08-30 12:26:21 +0600

    Image updated.

--- src/app/(home)/Contact.jsx
+++ src/app/(home)/Contact.jsx
@@ -20,7 +20,7 @@ const Contact = () => {
           <figure>
             <Image
               className="rounded-xl mx-4"
-              src="https://images.unsplash.com/photo-1571805529673-0f56b922b359?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGNoZWZ8ZW58MHx8MHx8&auto=format&fit=crop&w=300&q=60"
+              src="https://images.unsplash.com/photo-1473187983305-f615310e7daa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
               alt="Contact"
               width={600}
               height={400}
