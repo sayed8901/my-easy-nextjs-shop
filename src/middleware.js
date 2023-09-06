a0ea91100c87039a9ec3ae4e2a87b3ab9b780853
sayed8901 <sayed91515@gmail.com>
2023-09-07 01:10:21 +0600

    if a logged-in user wants to go the ‘login’ or ‘signup’ route, redirecting him to the homepage.

--- src/middleware.js
+++ src/middleware.js
@@ -5,6 +5,9 @@ import { NextResponse } from "next/server";
 export const middleware = async (request) => {
   const { pathname } = request.nextUrl;
 
+  // to check the current path মানে বর্তমানে কোন path এ আছে..
+  const isPath = (path) => pathname.startsWith(path);
+
   try {
     let cookie = request.cookies.get("jwt-token")?.value;
 
@@ -14,9 +17,19 @@ export const middleware = async (request) => {
     }
     const secret = new TextEncoder().encode(process.env.jwt_secret);
     await jwtVerify(cookie.split("Bearer ")[1], secret);
+
+    // যদি কোন user logged-in থাকে এবং সে যদি login বা signup এ পুনরায় যেতে চায়, তবে তাকে "/" মানে HomePage এ redirect করে দিয়ে দিবে
+    if (isPath("/login") || isPath("/signup")) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
     return NextResponse.next();
   } catch (error) {
     console.log(error.message);
+    
+    // login বা signup এর ক্ষেত্রে যদি কোন error হয়ে যায়, তাহলে login বা signup করতে না পারার কারনে next() কে call করে দিতে হবে
+    if (isPath("/login") || isPath("/signup")) {
+      return NextResponse.next();
+    }
     return NextResponse.redirect(
       new URL(`/login?redirectURL=${pathname}`, request.url)
     );
